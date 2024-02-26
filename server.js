42a14fdf1cdb0e6b67a05bd3d648054da4c24090
Glitch (glitch-hello-node) <none>
2024-02-26 05:01:17 +0000

    🚡🚁 Checkpoint ./server.js:64481259/259

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (request, response) => response.send('Welcome to the Booklist app!'));
+
+app.listen(port, () => console.log(`Express app running on port ${port}!`));
