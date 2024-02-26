44c317c545382c880a274a389fcd72438a5f1655
Glitch (glitch-hello-node) <none>
2024-02-26 05:25:15 +0000

    🚅🏑 Checkpoint ./server.js:64481259/389

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/dashboard', (request, response) => response.send('Playlist App Dashboard'));
+app.get('/homepage', (request, response) => response.send('Booklist App Homepage'));
+app.get('/dashboard', (request, response) => response.send('Booklist App Dashboard'));
 app.get('/', (request, response) => response.send('Welcome to the Booklist app!'));
 
 app.listen(port, () => console.log(`Express app running on port ${port}!`));
