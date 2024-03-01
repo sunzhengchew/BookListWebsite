608e043c86d7cfa715df4dc61bb519f9ead3992d
Glitch (glitch-hello-node) <none>
2024-03-01 11:16:37 +0000

    😀🌗 Checkpoint ./server.js:64481259/964 ./route.js:64481259/599

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 'use strict';
 
+import { create } from 'express-handlebars';
 import express from 'express';
+import routes from './routes.js'; 
+import logger from "./utils/logger.js";
 
 const app = express();
 const port = 3000;
 
-app.get('/homepage', (request, response) => response.send('Booklist App Homepage'));
-app.get('/dashboard', (request, response) => response.send('Booklist App Dashboard'));
-app.get('/collection', (request, response) => response.send('Booklist Collection'));
-app.get('/about', (request, response) => response.send('About this app'));
+app.use(express.static("public"));
 
-app.listen(port, () => console.log(`Express app running on port ${port}!`));
+const handlebars = create({extname: '.hbs'});
+app.engine(".hbs", handlebars.engine);
+app.set("view engine", ".hbs")
+
+app.use("/", routes);
+
+app.listen(port, () => logger.info("Your app is listening on port " + port));
+
