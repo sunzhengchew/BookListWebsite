c99c28e56f02aed556ff1195b2f93d904729ea9f
Glitch (glitch-hello-node) <none>
2024-03-03 01:44:39 +0000

    🐋🐆 Checkpoint ./views/booklist.hbs:64481259/138 ./routes.js:64481259/253 ./controllers/booklist.js:64481259/1161 ./models/mycollection.js:64481259/133 ./views/partials/listbooks.hbs:64481259/574

--- controllers/booklist.js
+++ controllers/booklist.js
@@ -1,12 +1,18 @@
 'use strict';
 
 import logger from '../utils/logger.js';
+import booklistStore from '../models/mycollection.js';
 
 const booklist = {
   createView(request, response) {
+    const booklistId = request.params.id;
+    logger.debug('Booklist id = ' + booklistId);
+    
     const viewData = {
-      title: 'Playlist'
+      title: 'Booklist',
+      singlePlaylist: booklistStore.getBooklist(booklistId)
     };
+
     response.render('booklist', viewData);
   },
 };
