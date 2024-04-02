0cc6db1e2f97a5f47ad6b8007163b6b97ae6d146
Glitch (sunzhengchewca1) <none>
2024-04-02 17:16:09 +0000

    🙉🎑 Checkpoint ./controllers/start.js:64481259/158

--- controllers/start.js
+++ controllers/start.js
@@ -1,11 +1,13 @@
 'use strict';
 import logger from "../utils/logger.js";
 import creator from "../models/info.js";
+import booklistStore from '../models/mycollection.js';
 
 const start = {                                // Creating an object named 'start' which contains methods 'createView'
   createView(request, response) {
     logger.info("Start page loading!");
-    
+    const booklists = booklistStore.getAllBooklists();
+
     const viewData = {
       title: "Welcome to the Booklist app!",
       info: creator.getAppInfo() // Retrieving application information using the 'getAppInfo' method from info.js
