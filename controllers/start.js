e04d1db9f483bf3af9f38c15fb9ee2d50aeb2e94
Glitch (sunzhengchewca1) <none>
2024-04-02 22:46:13 +0000

    🐙🎏 Checkpoint ./controllers/start.js:64481259/133

--- controllers/start.js
+++ controllers/start.js
@@ -7,10 +7,12 @@ const start = {                                // Creating an object named 'star
   createView(request, response) {
     logger.info("Start page loading!");
     const booklists = booklistStore.getAllBooklists();
-
+    let numBooklists = booklists.length;
+    
     const viewData = {
       title: "Welcome to the Booklist app!",
-      info: creator.getAppInfo() // Retrieving application information using the 'getAppInfo' method from info.js
+      info: creator.getAppInfo(), // Retrieving application information using the 'getAppInfo' method from info.js
+      displayNumBooklists: numBooklists,
     };
     
     //logger.debug(viewData);
