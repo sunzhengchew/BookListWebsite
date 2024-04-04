b26836dfc6934228fc768ccf25d98156a86f256b
Glitch (sunzhengchewca2) <none>
2024-04-04 18:07:52 +0000

    🍎👻 Checkpoint ./controllers/start.js:64481259/859 ./views/start.hbs:64481259/436

--- controllers/start.js
+++ controllers/start.js
@@ -8,11 +8,20 @@ const start = {                                // Creating an object named 'star
     logger.info("Start page loading!");
     const booklists = booklistStore.getAllBooklists();
     let numBooklists = booklists.length;
+    let numBooks = 0;
+    
+    for (let item of booklists) {
+      numBooks += item.books.length;
+    }
+    if(numBooks < 10){
+        numBooks
+    }
     
     const viewData = {
       title: "Welcome to the Booklist app!",
       info: creator.getAppInfo(), // Retrieving application information using the 'getAppInfo' method from info.js
       displayNumBooklists: numBooklists,
+      displayNumBooks:numBooks
     };
     
     //logger.debug(viewData);
