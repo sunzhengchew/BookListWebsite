17363e77e427fe22f667682258b9da7aaa344b59
Glitch (sunzhengchewca2) <none>
2024-04-21 15:41:24 +0000

    🚔👕 Checkpoint ./package.json:64481259/56 ./server.js:64481259/166 ./controllers/picklist.js:64481259/150 ./controllers/dashboard.js:64481259/544

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,6 +2,7 @@
 
 import logger from "../utils/logger.js";
 import booklistStore from "../models/mycollection.js";
+import { v4 as uuidv4 } from 'uuid';
 
 const dashboard = {                          // Creating an object named 'dashboard' which contains a method 'createView'
   createView(request, response) {
@@ -16,6 +17,17 @@ const dashboard = {                          // Creating an object named 'dashbo
     
     response.render('dashboard', viewData);
   },
+  addbooklist(request, response) {
+    const booklistId = request.params.id;
+    const booklist = booklistStore.getBooklist(booklistId);
+    const newBook = {
+      id: uuidv4(),
+      title: request.body.title,
+      category: request.body.category,
+    };
+    booklistStore.addbooklist(playlistId, newSong);
+    response.redirect('/playlist/' + playlistId);
+},
 };
 
 export default dashboard;                    // Exporting the 'dashboard' object for external usage
