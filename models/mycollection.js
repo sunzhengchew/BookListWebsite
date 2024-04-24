c2bf5f51be4a30519ac026d578c67f125e7315c5
Glitch (sunzhengchewca2) <none>
2024-04-24 23:49:28 +0000

    🌞🚁 Checkpoint ./models/mycollection.js:64481259/24 ./controllers/dashboard.js:64481259/11

--- models/mycollection.js
+++ models/mycollection.js
@@ -80,8 +80,8 @@ const booklistStore = {
     this.store.editItem(this.collection, id, bookId, this.array, updatedBook);
     response();
 },
-  editBooklist(updatebooklist) {
-    this.store.editItem(this.collection, updatebooklist);
+  editBooklist(id,updatebooklist) {
+    this.store.editCollection(this.collection,id, updatebooklist);
 },
 };
 
