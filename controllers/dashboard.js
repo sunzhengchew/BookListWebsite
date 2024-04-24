f85ebcbe93528120f80ce27074d01bf9ba92c1a6
Glitch (sunzhengchewca2) <none>
2024-04-24 23:06:11 +0000

    🚃👟 Checkpoint ./controllers/dashboard.js:64481259/51 ./models/mycollection.js:64481259/28

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -34,12 +34,12 @@ const dashboard = {                          // Creating an object named 'dashbo
 },
   updateBooklist(request, response) {
     const booklistId = request.params.id;
-    logger.debug("updating song " + booklistId);
+    logger.debug("updating booklist " + booklistId);
     const updatedBooklist = {
       id: booklistId,
       category: request.body.category
     };
-    booklistStore.editBooklist(updatedBooklist);
+    booklistStore.editBooklist(booklistId,updatedBooklist);
     response.redirect('/dashboard/');
 },
 };
