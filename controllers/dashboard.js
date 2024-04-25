5691e1769662438353d1e4074e3555a315ce9297
Glitch (sunzhengchewca2) <none>
2024-04-25 01:16:21 +0000

    Rewound to commit 108b7f813af771b9b5c2269404d0a94f2baafbe4 + 0 edits

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -38,8 +38,8 @@ const dashboard = {                          // Creating an object named 'dashbo
     const updatedCategory = request.body.category;
     const booklist = booklistStore.getBooklist(booklistId);
     const updatedBooklist = {
-        category: updatedCategory,
-        books:[],
+        ...booklist,
+        category: updatedCategory
     };
     booklistStore.editBooklist(booklistId, updatedBooklist);
     response.redirect('/dashboard/');
