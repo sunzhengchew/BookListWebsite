8ed82fee9ab94431342e8492fbada79255eeaa00
Glitch (sunzhengchewca2) <none>
2024-04-25 17:33:47 +0000

    👩🍣 Checkpoint ./controllers/booklist.js:64481259/194

--- controllers/booklist.js
+++ controllers/booklist.js
@@ -4,6 +4,8 @@ import logger from '../utils/logger.js';
 import booklistStore from '../models/mycollection.js';
 import { v4 as uuidv4 } from 'uuid';
 
+const sharp = require('sharp');
+
 const booklist = {                         // Creating an object named 'booklist' which contains a method 'createView'
   createView(request, response) {         // Method to handle rendering the 'booklist' view
     const booklistId = request.params.id;
@@ -20,17 +22,18 @@ const booklist = {                         // Creating an object named 'booklist
     const booklistId = request.params.id;
     const booklist = booklistStore.getBooklist(booklistId);
     const imageFile = request.files.image;
+    const imagePath ='models/mycollection.json';
     const maxWidth = 400;
     const maxHeight = 300;
-
     sharp(imageFile.data) // Use sharp to process the image data
-        .resize({ width: maxWidth, height: maxHeight, fit: 'inside' }) // Resize image to fit within maxWidth and maxHeight
-        .toFile(`${imagePath}/${imageFile.name}`, (err, info) => {
+    .resize({ width: maxWidth, height: maxHeight, fit: 'inside' }) // Resize image to fit within maxWidth and maxHeight
+    .toFile(`${imagePath}/${imageFile.name}`, (err, info) => {
             if (err) {
                 console.error('Error resizing image:', err);
                 // Handle error appropriately
                 return;
-            }
+        }
+    };
     const newBook = {
       id: uuidv4(),
       name: request.body.name,
@@ -42,7 +45,8 @@ const booklist = {                         // Creating an object named 'booklist
     booklistStore.addBook(booklistId, newBook,function(){
     response.redirect('/booklist/' + booklistId);
     });
- },
+},
+          
   deleteBook(request, response) {
     const booklistId = request.params.id;
     const bookId = request.params.bookid;
